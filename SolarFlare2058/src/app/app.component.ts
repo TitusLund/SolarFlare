f6b0088a9904bb5d0b8d6c658f48640a4e4370f8
JacobSams <jacobsams427@gmail.com>
2025-10-25 00:50:48 -0700

    Placeholder board

--- SolarFlare2058/src/app/app.component.ts
+++ SolarFlare2058/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   currentScore = 0;
+  gameBoard: number[] = [0,2,0,0,
+                         0,0,0,0,
+                         0,0,0,2,
+                         0,0,0,0];
 }
