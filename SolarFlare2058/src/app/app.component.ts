4c75227d511faa32abdc87c309fd66a13f615b80
Tyler <tfriesen2@my.gcu.edu>
2025-10-27 16:38:43 -0700

    created leaderboard component

--- SolarFlare2058/src/app/app.component.ts
+++ SolarFlare2058/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor, NgIf, NgClass } from '@angular/common';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NgIf, NgClass],
+  imports: [RouterOutlet, NgFor, NgIf, NgClass, LeaderboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   currentScore = 0;
-  gameBoard: number[] = [0,2,0,0,
-                         0,0,0,0,
-                         0,0,0,2,
-                         0,0,0,0];
+  gameBoard: number[] = [0, 2, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 2,
+    0, 0, 0, 0];
 }
