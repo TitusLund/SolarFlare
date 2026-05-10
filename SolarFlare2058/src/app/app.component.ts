60fbdf4a15893b2742c8cf4da6843b4509583314
TitusLund <tlund365@gmail.com>
2026-05-10 14:48:01 -0700

    changed urls

--- SolarFlare2058/src/app/app.component.ts
+++ SolarFlare2058/src/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent {
     this.isGameOverOpen = false;
     // TODO: remove the hardcoded localhost here
     this.http
-      .post('http://localhost:3000/api/game/start', {})
+      .post('http://app.attem.xyz/api/game/start', {})
       .subscribe({
         next: (response: any) => {
           this.gameId = response.game.id;
@@ -108,7 +108,7 @@ export class AppComponent {
     if (this.isGameOverOpen) return;
 
     this.http
-      .post('http://localhost:3000/api/game/shift', {
+      .post('http://app.attem.xyz/api/game/shift', {
         gameId: this.gameId,
         direction: direction,
       })
--- SolarFlare2058/src/app/game-over/game-over.component.ts
+++ SolarFlare2058/src/app/game-over/game-over.component.ts
@@ -36,7 +36,7 @@ export class GameOverComponent {
      * need to fix.
      */
 
-    this.http.post('http://localhost:3000/api/scores/saveScore', {
+    this.http.post('http://app.attem.xyz/api/scores/saveScore', {
       username: this.username,
       score: this.score
     }).subscribe({
